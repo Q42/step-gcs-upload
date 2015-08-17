51dfaab8731a53510af1cc260214746d52ceca01
Mark <markj@q42.nl>
2015-08-17 17:03:52 +0200

    Added go gets to step.sh and removed the other stuff

--- step.sh
+++ step.sh
@@ -5,6 +5,9 @@ THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Start go program
 cd "${THIS_SCRIPTDIR}"
 
+go get -u golang.org/x/net/context
+go get -u golang.org/x/oauth2/google
+go get -u google.golang.org/api/storage/...
 go run ./program.go
 ex_code=$?
 
